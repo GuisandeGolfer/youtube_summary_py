145555cb5e92589948253619453b7d5cadd570e9
GuisandeGolfer <DGuisande873@gmail.com>
2024-08-27 19:52:58 -0700

    with claude: reworked script to be more clear with argparse

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
 import os
 import subprocess
 import time
+import logging
+from typing import Tuple
 from dotenv import load_dotenv
 from openai import OpenAI
 from tqdm import tqdm
+import argparse
 
+# Load environment variables
 load_dotenv()
 
-dest_path = os.path.abspath("/Users/diegoguisande/Desktop/PARA/Projects_1/youtube_summary_py")
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Constants
+DEST_PATH = os.path.abspath("/Users/diegoguisande/Desktop/PARA/Projects_1/youtube_summary_py")
+AUDIO_PATH = os.path.join(DEST_PATH, "audio")
+TRANSCRIPTION_PATH = os.path.join(DEST_PATH, "transcriptions")
+OBSIDIAN_BASE_PATH = "/Users/diegoguisande/Library/Mobile Documents/iCloud~md~obsidian/Documents/Second Brain/PARA"
 
 
-def pick_dest_folder() -> int:
-    para_folder: str = input(
-        "\n Where do you want to put the file? (Projects (1), Areas (2), Resources (3), Archives (4))\n"
-    )
-    para_choice = int(para_folder)
-    if type(para_choice) is not int:
-        raise Exception("\n don't add .mp3, just the filename")
-    else:
-        return para_choice
-
-
-def recieve_filename() -> str:
-    filename: str = input("what do you want the summary/audio to be named?... (don't put extension)\n")
-    if ".mp3" not in filename:
-        return filename
-    else:
-        raise Exception("\n don't add .mp3, just the filename")
-
-
-def recieve_video_url() -> str:
-    print("hello! Welcome to the Youtube-Summary-Tool")
-    video: str = input("Please enter a video url... \n")
-    if "https://youtu.be" in video or "youtube" in video:
-        return video
-    else:
-        raise Exception("not a url \n needs .com or www")
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="YouTube Video Summarizer")
+    parser.add_argument("--url", help="YouTube video URL", required=True)
+    parser.add_argument("--filename", help="Output filename (without extension)", required=True)
+    parser.add_argument("--dest", type=int, choices=[1, 2, 3, 4], help="Destination folder (1: Projects, 2: Areas, 3: Resources, 4: Archives)", required=True)
+    parser.add_argument("--keep", help="if you want to keep the audio and raw transcription")
+    return parser.parse_args()
 
 
 def download_video_audio(url: str, filename: str) -> str:
-    os.chdir("/Users/diegoguisande/Desktop/PARA/Projects_1/youtube_summary_py/audio")
-    current_pth = os.getcwd()
-    if os.path.exists(f'{current_pth}/{filename}.mp3'):
-        raise Exception("\n There is a file with the same name in the audio folder")
-    bash_command = f"yt-dlp --progress -x --audio-format mp3 --output {filename}.mp3 {url}"
+    os.makedirs(AUDIO_PATH, exist_ok=True)
+    output_path = os.path.join(AUDIO_PATH, f"{filename}.mp3")
 
-    # Execute the bash command and capture the output
+    if os.path.exists(output_path):
+        raise FileExistsError(f"File {output_path} already exists")
+
+    bash_command = f"yt-dlp --progress -x --audio-format mp3 --output {output_path} {url}"
+
     with tqdm(total=100, bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt}") as pbar:
         pbar.set_description("Downloading video")
         process = subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -54,37 +47,35 @@ def download_video_audio(url: str, filename: str) -> str:
             if process.poll() is not None and output == '':
                 break
             if output:
-                print(output.strip())
-                pbar.update(10)  # Simulate progress update, adjust as necessary
-            time.sleep(0.5)  # Simulate delay
+                logging.info(output.strip())
+                pbar.update(10)  # Simulate progress update
+            time.sleep(0.5)
         pbar.update(100 - pbar.n)  # Ensure the bar completes
-
-    # delete mp3
-    os.remove(f'{current_pth}/{filename}.mp3')
 
     return filename
 
 
-def transcribe_mp3_file(filename: str, dest_path=dest_path) -> tuple[OpenAI, str]:
+def transcribe_mp3_file(filename: str) -> Tuple[OpenAI, str]:
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    audio_file = open(f"{dest_path}/audio/{filename}.mp3", "rb")
+    audio_file_path = os.path.join(AUDIO_PATH, f"{filename}.mp3")
 
-    with tqdm(total=100, bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt}") as pbar:
+    with open(audio_file_path, "rb") as audio_file, tqdm(total=100, bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt}") as pbar:
         pbar.set_description("Transcribing audio")
+        # TODO: add a spinning ASCII wheel instead of using tqdm
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file,
         )
-        pbar.update(100)  # Complete the progress bar
+        pbar.update(100)
 
-    with open(f"{dest_path}/transcriptions/{filename}.txt", 'w') as file:
+    os.makedirs(TRANSCRIPTION_PATH, exist_ok=True)
+    with open(os.path.join(TRANSCRIPTION_PATH, f"{filename}.txt"), 'w') as file:
         file.write(transcription.text)
 
-    audio_file.close()
     return client, transcription.text
 
 
-def ask_gpt_for_summary(client, transcript: str, url: str) -> str:
+def ask_gpt_for_summary(client: OpenAI, transcript: str, url: str) -> str:
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -96,30 +87,33 @@ def ask_gpt_for_summary(client, transcript: str, url: str) -> str:
 
 
 def save_file_to_obsidian(location: int, transcript: str, filename: str) -> None:
-    local_obsidian_path = "/Users/diegoguisande/Library/Mobile Documents/iCloud~md~obsidian/Documents/Second Brain/PARA"
-    print(f"obsidian path: {local_obsidian_path}")
-    if location == 1:
-        local_obsidian_path = local_obsidian_path + "/Projects"
-    elif location == 2:
-        local_obsidian_path = local_obsidian_path + "/Areas"
-    elif location == 3:
-        local_obsidian_path = local_obsidian_path + "/Resources"
-    else:
-        local_obsidian_path = local_obsidian_path + "/Archives"
-    summary_text = str(transcript)
-    with open(f"{local_obsidian_path}/{filename}.md", "w") as ai_file:
-        ai_file.write(summary_text)
-    print(f"file saved to Obsidian!! inside of {local_obsidian_path}")
+    folder_names = {1: "Projects", 2: "Areas", 3: "Resources", 4: "Archives"}
+    folder_name = folder_names.get(location, "Archives")  # Default to Archives if invalid input
+    obsidian_path = os.path.join(OBSIDIAN_BASE_PATH, folder_name)
+
+    os.makedirs(obsidian_path, exist_ok=True)
+    with open(os.path.join(obsidian_path, f"{filename}.md"), "w") as ai_file:
+        ai_file.write(transcript)
+
+    logging.info(f"File saved to Obsidian inside of {obsidian_path}")
+
+    logging.info(f"deleting audio and raw transcription of {filename}")
+
+    os.remove(f"{AUDIO_PATH}/{filename}.mp3")
+    os.remove(f"{TRANSCRIPTION_PATH}/{filename}.txt")
 
 
 def main():
-    url = recieve_video_url()
-    filename = recieve_filename()
-    obsidian_dest = pick_dest_folder()
-    file_name = download_video_audio(url, filename)
-    client, transcription = transcribe_mp3_file(file_name)
-    summary = ask_gpt_for_summary(client, transcription, url)
-    save_file_to_obsidian(obsidian_dest, summary, file_name)
+    args = parse_arguments()
+
+    try:
+        file_name = download_video_audio(args.url, args.filename)
+        client, transcription = transcribe_mp3_file(file_name)
+        summary = ask_gpt_for_summary(client, transcription, args.url)
+        save_file_to_obsidian(args.dest, summary, file_name)
+        logging.info("Process completed successfully")
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
 
 
 if __name__ == "__main__":
